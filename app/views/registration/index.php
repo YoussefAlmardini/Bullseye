99097e4143f02fbf7bfeeaedad1dafa4b553fa4f
Miquel <miquelalessandro@gmail.com>
2019-12-09 10:23:20 +0100

    update aan registratie pagina

--- app/views/registration/index.php
+++ app/views/registration/index.php
@@ -1,24 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <title>Registreren - NL Rangers</title>
-    <link rel="stylesheet" href="registration.css">
+    <link rel="stylesheet" type="text/css" href="../src/styles/generalStyles.css">
+    <link rel="stylesheet" type="text/css" href="../src/styles/registreren.css">
 </head>
 
 <body>
-    <form action="/registration/catchData" method="POST">
-        <input required type="text" placeholder="Voornaam *" name="firstName"> <br><br>
-        <input type="text" placeholder="Tussenvoegsel" name="insertion"> <br><br>
-        <input required type="text" placeholder="Achternaam *" name="lastName"> <br><br>
-        <label for="birthDate">Geboortedatum: </label>
-        <input required type="date" name="birthDate"> <br><br>
-        <input required type="email" placeholder="E-mailadres *" name="email_address"> <br><br>
-        <input required type="email" placeholder="Herhaal e-mailadres *"> <br><br>
-        <input required type="password" placeholder="Wachtwoord *" name="password"> <br><br>
-        <input required type="password" placeholder="Herhaal wachtwoord *"> <br><br>
-        <input required type="submit" placeholder="Registreren" name="submit">
-    </form>
-</body>
+<div class="viewContainer viewContainerCustom">
 
+    <div class="topBlock">
+
+        <div class="Title">
+            NLRANGERS
+        </div>
+
+        <div class="notification">
+
+        </div>
+
+    </div>
+
+    <div class="middleBlock">
+        <div class="secondTitle formTitle">
+            Registreren
+        </div>
+
+        <form class="formregister" action="/registration/catchData" method="POST">
+33
+
+            <div class="inputContainer">
+                <input required type="text" placeholder="Voornaam *" name="firstName">
+            </div>
+            <div class="inputContainer">
+                <input type="text" placeholder="Tussenvoegsel" name="insertion">
+            </div>
+            <div class="inputContainer">
+                <input required type="text" placeholder="Achternaam *" name="lastName">
+            </div>
+            <div class="inputContainer">
+                <label class="birthdatetitle" for="birthDate">Geboortedatum: </label> <br><br>
+                <input required type="date" name="birthDate">
+            </div>
+
+            <div class="inputContainer">
+                <input placeholder="E-mail" type="email" />
+            </div>
+
+            <div class="inputContainer">
+                <input placeholder="E-mail herhalen" type="email" />
+            </div>
+
+            <div class="inputContainer">
+                <input placeholder="Wachtwoord" type="password" />
+            </div>
+
+            <div class="inputContainer">
+                <input placeholder="Wachtwoord herhalen" type="password" />
+            </div>
+
+            <div class="inputContainer">
+                <button type="submit">Verzenden</button>
+            </div>
+
+        </form>
+
+    </div>
+
+    <div class="bottomBlock">
+
+        <div class="secondTitle">Heeft u al een account?</div>
+        <div class="link"><a href="#">Inloggen</a></div>
+
+    </div>
+
+
+</div>
+
+
+</body>
 </html>
