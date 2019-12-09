77abd5b9f296e69c7d5a61f3b3c6ae62ff674770
TLSvdAvoort <somegamemusic@gmail.com>
2019-12-09 15:48:55 +0100

    Fix image routing Added files for main page user

--- app/views/map/mapload.php
+++ app/views/map/mapload.php
@@ -4,7 +4,9 @@
    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
    crossorigin=""/>
 </head>
-   
+<body>
+<div id="mymap"></div>
+
 <!-- Make sure you put this AFTER Leaflet's CSS -->
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 
@@ -29,7 +31,7 @@
 
             // Sets icon
             var myIcon = L.icon({
-                iconUrl: 'src/assets/myicon.png',
+                iconUrl: window.location.origin + '/src/assets/myicon.png',
                 iconSize: [30, 50],
                 iconAnchor: [20,55],
                 popupAnchor: [],
@@ -71,3 +73,4 @@
         });
     }
 </script>
+</body>
