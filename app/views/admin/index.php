becf3320f8965097466d511ba579517e3db14e64
Andy <496993@edu.rocmn.nl>
2019-12-10 12:23:24 +0100

    Commit admin  map

---
+++ app/views/admin/index.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+?>
+<html>
+
+<body>
+    <div id="mymap"></div>
+    <input type="text" id="title" value="">
+
+    <!-- Always as last loaded  -->
+</body>
+<?php
+include "mapload.php";
+?>
+
+
+</html>
