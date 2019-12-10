0b53ded8d3216873c5d4d515156ed0734030ddf9
YoussefAlmardini <yousef.mardini@gmail.com>
2019-12-10 11:36:21 +0100

    navigation bottom added

--- app/views/map/index.php
+++ app/views/map/index.php
@@ -2,23 +2,35 @@
 // include "header.php";
 ?>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../src/styles/generalStyles.css">
+        <link rel="stylesheet" type="text/css" href="../src/styles/map.css">
+        <link rel="stylesheet" type="text/css" href="../src/styles/bottomNavigation.css">
+    </head>
 
+    <body>
 
-</body>
-<div id="mymap"></div>
-    <input type="number" id="XcoordinateM" placeholder="X Coordinate Map" value="52.1844862">
-    <input type="number" id="YcoordinateM" placeholder="Y Coordinate Map" value="5.4061271">
-    <input type="button" onclick="getMap()" value="Laad map">
+        <div id="mymap"></div>
+        <input type="number" id="XcoordinateM" placeholder="X Coordinate Map" value="52.1844862">
+        <input type="number" id="YcoordinateM" placeholder="Y Coordinate Map" value="5.4061271">
+        <input type="button" onclick="getMap()" value="Laad map">
 
-    <input type="number" id="Xcoordinate" placeholder="X Coordinate Marker">
-    <input type="number" id="Ycoordinate" placeholder="Y Coordinate Marker">
-    <input type="text" id="PopupMessage" placeholder="Popup bericht">
-    <input type="button" onclick="getCoordinates()" value="Set a marker">
+        <input type="number" id="Xcoordinate" placeholder="X Coordinate Marker">
+        <input type="number" id="Ycoordinate" placeholder="Y Coordinate Marker">
+        <input type="text" id="PopupMessage" placeholder="Popup bericht">
+        <input type="button" onclick="getCoordinates()" value="Set a marker">
+
+        <?php include "../app/components/bottomNavigation/index.php"; ?>
+        <script>
+            BotBarNavigation.SwitchActivaty('map');
+        </script>
+    </body>
+    
+</html>
+
 
     <!-- Always as last loaded  -->
 <?php
 include "mapload.php";
 ?>
 
-
-</html>
---
+++ public/src/js/accountNavigation.js
@@ -0,0 +1,84 @@
+class BotBarNavigation{
+
+     static navItems =()=> {
+      return{
+        profile: {
+            element : document.getElementById('profile'),
+            active : false,
+            location : "profiel"
+        },
+        map:{
+            element : document.getElementById('map'),
+            active : true,
+            location : "map"
+        },
+        settings:{
+            element : document.getElementById('settings'),
+            active : false,
+            location : "settings"
+        },
+      }
+    }
+
+    static Navigate =(location)=>{
+      this.UpdateGraphics(location);
+    }
+
+    static UpdateGraphics=(element)=>{
+
+        let nav = this.navItems();
+
+        switch(element){
+            case Element.PROFILE:
+                    nav.profile.active = true;
+                    nav.map.active = false;
+                    nav.settings.active = false;
+                    this.SwitchActivaty(nav);
+                    this.LoadPage(nav);
+                    
+                break;
+            case Element.MAP:
+                    nav.profile.active = false;
+                    nav.map.active = true;
+                    nav.settings.active = false;
+                    this.SwitchActivaty(nav);
+                    this.LoadPage(nav);
+                break;
+            case Element.SETTINGS:
+                    nav.profile.active = false;
+                    nav.map.active = false;
+                    nav.settings.active = true;
+                    this.SwitchActivaty(nav);
+                    this.LoadPage(nav);
+                break;
+
+        }
+    }
+
+    static SwitchActivaty=(currentLocation)=>{
+        console.log(currentLocation);
+       for(let key in this.navItems()){
+           if(this.navItems()[key].location == currentLocation){
+            this.navItems()[key].element.classList.add('active');
+           }else{
+            this.navItems()[key].element.classList.remove('active');
+           }
+       }
+    }
+
+    static LoadPage=(nav)=>{
+        for(let key in nav){
+            if(nav[key].active){
+                window.location.replace("http://nlrangers.test/"+nav[key].location);
+            }
+        }
+       
+    }
+}
+
+const Element = {
+    PROFILE: 'profile',
+    MAP: 'map',
+    SETTINGS:'settings'
+}
+
