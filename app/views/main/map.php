a0593cea8dc41482135d3733e83c35fc270eb6f8
TLSvdAvoort <somegamemusic@gmail.com>
2019-12-11 09:56:49 +0100

    Added function that you can locate yourself with a button

--- app/views/main/map.php
+++ app/views/main/map.php
@@ -1,15 +1,16 @@
 <head>
     <link rel="stylesheet" href="../src/styles/map.css"/>
    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-   crossorigin=""/>
-   
+    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+    crossorigin=""/>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.css">
 </head>
 <body>
 <div id="mymap"></div>
 
 <!-- Make sure you put this AFTER Leaflet's CSS -->
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+<script src="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.js"></script>
 <script> 
     document.getElementById('mymap').innerHTML = "<div id='map' style='width: 100%; height: 90%;'></div>";
     const mymap = L.map('mymap').setView([52.1637739, 5.3965879], 15);
@@ -38,6 +39,11 @@
         clickable: true,            // Make the icon clickable
         alt: 'mymarker'}            // Name for accessibillity
     );
+
+    L.easyButton('<span class="bigdot">&bigodot;</span>', function(){
+        mymap.setView(mymarker.getLatLng(), 18);
+    }).addTo(mymap);
+    
     mymap.locate({ setView:true, watch: true });
     mymap.on('locationfound', onLocationFound);
     
