6e61f12d4d05a4717c66a888fc909b3b438083b8
Miquel <miquelalessandro@gmail.com>
2020-01-08 09:45:54 +0100

    profiel pagina af

--- app/views/profiel/index.php
+++ app/views/profiel/index.php
@@ -5,7 +5,7 @@
         <div class="viewContainer viewContainerCustom">
 
             <div class="topBlock">
-
+                <br><br><br>
                 <div class="secondTitle formTitle">
                     Profiel
                 </div>
@@ -20,19 +20,19 @@
 
                     <div class="inputContainer marginset">
                         <label class="birthdatetitle" for="birthDate">Voornaam: </label>
-                        <input required type="text" placeholder="Voornaam *" id="inputs" name="firstName">
+                        <input class="firstname" required type="text" placeholder="Voornaam *" value="" id="firstname" name="firstName">
 
                     </div>
 
                     <div class="inputContainer">
                         <label class="birthdatetitle" for="birthDate">Tussenvoegsel: </label>
-                        <input type="text" placeholder="Tussenvoegsel" id="inputs" name="insertion">
+                        <input class="insertion" type="text" placeholder="Tussenvoegsel" id="insertion" name="insertion">
 
                     </div>
 
                     <div class="inputContainer">
                         <label class="birthdatetitle" for="birthDate">Achternaam: </label>
-                        <input required type="text" placeholder="Achternaam *" id="inputs"  name="lastName">
+                        <input class="lastname" required type="text" placeholder="Achternaam *" id="lastname"  name="lastName">
 
 
                     </div>
@@ -40,20 +40,20 @@
 
                     <div class="inputContainer">
                         <label class="birthdatetitle" for="birthDate">E-mail: </label>
-                        <input required type="email" placeholder="E-mail *" readonly id="inputs" readonly name="email">
+                        <input class="email" required type="email" placeholder="E-mail *" disabled readonly id="email" readonly name="email">
                         \
                     </div>
 
                     <div class="inputContainer">
                         <label class="birthdatetitle" for="birthDate">Level: </label>
-                        <input required type="text" placeholder="Level *" id="inputs" readonly name="level">
+                        <input class="level" required type="text" placeholder="Level *" disabled id="level" readonly name="level">
 
                     </div>
 
 
                     <div class="inputContainer">
                         <label class="birthdatetitle" for="birthDate">Geboortedatum: </label>
-                        <input required type="date" id="inputs" name="birthdate">
+                        <input class="birthdate" required type="date" id="birthdate" name="birthdate">
 
                     </div>
 
@@ -75,6 +75,16 @@
 </html>
 
 <script>
+
+    document.getElementById('firstname').value = "<?php echo $data['user']['first_name']; ?>";
+    document.getElementById('insertion').value = "<?php echo $data['user']['insertion']; ?>";
+    document.getElementById('lastname').value = "<?php echo $data['user']['last_name']; ?>";
+    document.getElementById('email').value = "<?php echo $data['user']['email_address']; ?>";
+    document.getElementById('level').value = "<?php echo $data['user']['level']; ?>";
+    document.getElementById('birthdate').value = "<?php echo $data['user']['birthdate']; ?>";
+
+
+
     function Edit(e) {
         let targetInput = e.parentElement.children[1];
         let targetImage = e.parentElement.children[2];
