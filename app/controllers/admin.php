9d0d49f68b7d476e2ace12ffd3246860c8530379
Ertan Arslan <royallaltinn@gmail.com>
2020-01-14 12:29:27 +0100

    Revert unwanted changes in previous commits

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -59,5 +59,52 @@ class Admin extends Controller
     public function addCustomer(){
         return $this->view('admin/addCustomer');
     }
+
+    public function addOrganisation(){
+        $customers = [];
+        $query = 'SELECT name FROM customers;';
+        $db = DB::connect();
         $stmt = $db->prepare($query);
+        $stmt->execute();
+        $res = $stmt->fetchAll();
+
+        for($i = 0; $i < count($res); $i++){
+            array_push($customers, $res[$i]['name']);
+        }
+        
+        return $this->view('admin/addOrganisation', ['customers' => $customers]);
+    }
+
+    public function sendCustomerDataToModel(){
+        if(isset($_POST['submit'])){
+            $companyName = $_POST['company_name'];
+            $postalCode = $_POST['postal_code'];
+            $streetName = $_POST['street_name'];
+            $houseNumber = $_POST['house_number'];
+            $houseLetter = $_POST['house_letter'];
+            $mailingAddressPostalCode = $_POST['mailing_address_postal_code'];
+            $mailingAddressStreetName = $_POST['mailing_address_street_name'];
+            $mailingAddressHouseNumber = $_POST['mailing_address_house_number'];
+            $mailingAddressHouseLetter = $_POST['mailing_address_house_letter'];
+
+            $this->model('Customer')->saveCustomer($companyName, $postalCode, $streetName, $houseNumber, $houseLetter, $mailingAddressPostalCode, $mailingAddressStreetName, $mailingAddressHouseNumber, $mailingAddressHouseLetter);
+        }
+    }
+
+    public function sendOrganisationDataToModel(){
+        if(isset($_POST['submit'])){
+            $customer = $_POST['customer'];
+            $organisationName = $_POST['organisation_name'];
+            $postalCode = $_POST['postal_code'];
+            $streetName = $_POST['street_name'];
+            $houseNumber = $_POST['house_number'];
+            $houseLetter = $_POST['house_letter'];
+            $mailingAddressPostalCode = $_POST['mailing_address_postal_code'];
+            $mailingAddressStreetName = $_POST['mailing_address_street_name'];
+            $mailingAddressHouseNumber = $_POST['mailing_address_house_number'];
+            $mailingAddressHouseLetter = $_POST['mailing_address_house_letter'];
+
+            $this->model('Organisation')->saveOrganisation($customer, $organisationName, $postalCode, $streetName, $houseNumber, $houseLetter, $mailingAddressPostalCode, $mailingAddressStreetName, $mailingAddressHouseNumber, $mailingAddressHouseLetter);
+        }
+    }
 }
