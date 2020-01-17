2d229269b8e13ee7b24ed5b4c6cd90b2ce92d50c
Ertan Arslan <royallaltinn@gmail.com>
2020-01-17 11:07:11 +0100

    Profile of admin, customer and organisation can now be edited

--- app/views/admin/updateProfile.php
+++ app/views/admin/updateProfile.php
@@ -11,22 +11,20 @@ if (!$_SESSION['adminLoggedIn'] && !$_SESSION['customerLoggedIn'] && !$_SESSION[
     </head>
 
     <body>
-        <form action="/admin/sendProfileDataToModel">
+        <form action="/admin/sendProfileDataToModel" method="post">
             <label>Voornaam: </label><br>
-            <input type="text" name="firstName" value="<?php $data['profileData']['first_name'] ?>"><br>
+            <input type="text" name="firstName" value="<?php echo $data['profileData'][0][0] ?>"><br>
 
             <label>Tussenvoegsel: </label><br>
-            <input type="text" name="insertion" value="<?php $data['insertion'] ?>"><br>
+            <input type="text" name="insertion" value="<?php echo $data['profileData'][0][1] ?>"><br>
 
             <label>Achternaam: </label><br>
-            <input type="text" name="lastName" value="<?php $data['last_name'] ?>"><br>
+            <input type="text" name="lastName" value="<?php echo $data['profileData'][0][2] ?>"><br>
 
             <label>E-mailadres: </label><br>
-            <input type="email" name="email" value="<?php $data['email_address'] ?>"><br><br>
+            <input type="email" name="email" value="<?php echo $data['profileData'][0][3] ?>"><br><br>
 
             <input type="submit" name="submit">
         </form>
     </body>
-    
-    <!-- <?php var_dump($data['profileData']); ?> -->
 </html>
